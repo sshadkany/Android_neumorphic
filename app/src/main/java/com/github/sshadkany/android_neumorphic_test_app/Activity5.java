f6eb315b9bd041550e98ee7eb2cb38e90c14022b
sshakany <sshadkany@gmail.com>
2020-04-28 22:01:03 +0430

    Activity 5 with another design

---
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/Activity5.java
@@ -0,0 +1,14 @@
+package com.github.sshadkany.android_neumorphic_test_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+    }
+}
