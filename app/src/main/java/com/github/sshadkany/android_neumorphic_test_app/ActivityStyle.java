f638f59cecbedbbeb9f4b2aed2f2582c77133777
sshakany <sshadkany@gmail.com>
2020-04-28 19:25:06 +0430

    add another example and activity

---
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/ActivityStyle.java
@@ -0,0 +1,13 @@
+package com.github.sshadkany.android_neumorphic_test_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityStyle extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style);
+    }
+}
