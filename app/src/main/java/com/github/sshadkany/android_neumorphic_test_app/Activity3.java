70f8a91267eaab61d755a0dcfeecd3b802914e90
sshakany <sshadkany@gmail.com>
2020-04-28 18:25:28 +0430

    adding Activity3 for nesting tutorial

---
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/Activity3.java
@@ -0,0 +1,14 @@
+package com.github.sshadkany.android_neumorphic_test_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
