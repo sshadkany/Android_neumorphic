83f885a25e0432dfd239b2aa5cf7fa620700ff00
sshakany <sshadkany@gmail.com>
2020-06-04 09:50:30 +0430

    example for class Rect button and dark mode _ button making is now easier .

--- app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/RectButtonActivity.java
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/RectButtonActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.github.sshadkany.RectButton;
+
 public class RectButtonActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +15,10 @@ public class RectButtonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rect_button);
 
-        View recButton = findViewById(R.id.rec_image_button);
+        RectButton recButton = findViewById(R.id.rec_image_button);
+
+        findViewById(R.id.backgroundLayout).setBackgroundColor(recButton.background_color);  // set layout background color from neo object.
+
         recButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
