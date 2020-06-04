7b2201812ccc1146023e441f0b3289de500cedef
sshakany <sshadkany@gmail.com>
2020-06-04 11:45:44 +0430

    Dark mode example

--- app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/SecendActivity.java
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/SecendActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.github.sshadkany.PolygonButton;
+import com.github.sshadkany.RectButton;
 import com.github.sshadkany.neo;
 import com.github.sshadkany.shapes.CircleView;
 import com.github.sshadkany.shapes.PolygonView;
+import com.github.sshadkany.shapes.RoundRectView;
 
 public class SecendActivity extends AppCompatActivity {
 
@@ -17,43 +20,40 @@ public class SecendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secend);
-
+////*************************below code changes sides of polygon when its clicked ***************************************
 //        final PolygonView my_polygon = findViewById(R.id.my_polygon);
-//        my_polygon.setOnClickListener(new View.OnClickListener() {
+//        my_polygon.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
-//            public void onClick(View v) {
-//                int side = my_polygon.getSide();
-//                if (side > 12){
-//                    my_polygon.setSide(3);
-//                }else {
-//                    my_polygon.setSide(side+1);
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (my_polygon.isShapeContainsPoint(event.getX(), event.getY())) {
+//                    switch (event.getAction()) {
+//                        case MotionEvent.ACTION_DOWN:
+//                            // PRESSED
+//                            int side = my_polygon.getSide();
+//                            if (side > 12){
+//                                my_polygon.setSide(3);
+//                            }else {
+//                                my_polygon.setSide(side+1);
+//                            }
+//                            return true;
+//                        case MotionEvent.ACTION_UP:
+//                        case MotionEvent.ACTION_CANCEL:
+//                            // RELEASED
+//                            return true;
+//                    }
+//                } else {
+//                    // RELEASED
+//                    return true;
 //                }
+//                return false;
 //            }
 //        });
+//************************************************************************************
 
-        final neo mybtn = findViewById(R.id.my_button);
-        mybtn.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // is shape Contains Point ----> for detect place of Touch is in the shape or not
-                if (mybtn.isShapeContainsPoint(event.getX(), event.getY())) {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-                            // PRESSED
-                            //use only "small inner shadow" because its same size with "drop shadow" style and "big inner shadow" is bigger
-                            // "small inner shadow" = "drop shadow"
-                            // "big inner shadow"  > "drop shadow"
-                            mybtn.setStyle(neo.small_inner_shadow);
-                            return true; // if you want to handle the touch event
-                        case MotionEvent.ACTION_UP:
-                        case MotionEvent.ACTION_CANCEL:
-                            // RELEASED
-                            mybtn.setStyle(neo.drop_shadow);
-                            return true; // if you want to handle the touch event
-                    }
-                }
-                return false;
-            }
-        });
+
+        RoundRectView rectView = findViewById(R.id.roundRectView);
+        findViewById(R.id.backgroundLayout).setBackgroundColor(rectView.background_color);  // set layout background color from neo object.
+
+
     }
 }
