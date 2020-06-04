a82abe01d34ab4a3428b6c8278e91e304903c3db
sshakany <sshadkany@gmail.com>
2020-06-04 10:14:28 +0430

    example for class polygon button _ button making is now easier .

---
+++ app/src/main/java/com/github/sshadkany/android_neumorphic_test_app/PolygonButtonActivity.java
@@ -0,0 +1,29 @@
+package com.github.sshadkany.android_neumorphic_test_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.github.sshadkany.PolygonButton;
+
+public class PolygonButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.github.sshadkany.android_neumorphic_test_app.R.layout.activity_polygon_button);
+
+        PolygonButton polygonButton = findViewById(com.github.sshadkany.android_neumorphic_test_app.R.id.polygon_button);
+
+        findViewById(com.github.sshadkany.android_neumorphic_test_app.R.id.backgroundLayout).setBackgroundColor(polygonButton.background_color);  // set layout background color from neo object.
+
+        polygonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("neo", "onClick: I am poly button !!");
+            }
+        });
+    }
+}
